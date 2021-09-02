34ea18506e3afad4aafae481a611a03edb58cec2
yamashi <yamashi@users.noreply.github.com>
2021-09-03 00:06:17 +0200

    Fix xmake

--- xmake.lua
+++ xmake.lua
@@ -7,18 +7,18 @@ set_arch("x64")
 target("cppio")
     set_kind("static")
     add_files("src/*.cpp")
-    add_headerfiles("src/*.h", "src/*.inl")
+    add_headerfiles("src/*.hpp", "src/*.inl")
     add_includedirs("src/", {public = true})
     add_includedirs("vendor/outcome/single-header", {public = true})
 
     if is_plat("windows") then
         add_files("src/win32/*.cpp")
-        add_headerfiles("src/win32/*.h", "src/win32/*.inl")
+        add_headerfiles("src/win32/*.hpp", "src/win32/*.inl")
 
         add_syslinks("ws2_32", "Mswsock")
     else
         add_files("src/posix/*.cpp")
-        add_headerfiles("src/posix/*.h", "src/posix/*.inl")
+        add_headerfiles("src/posix/*.hpp", "src/posix/*.inl")
 
         add_syslinks("pthread")
     end
@@ -27,6 +27,6 @@ target("cppio")
 target("tests")
     set_kind("binary")
     add_files("tests/**.cpp")
-    add_headerfiles("tests/**.h")
+    add_headerfiles("tests/**.hpp")
     add_includedirs("tests/")
     add_deps("cppio")
